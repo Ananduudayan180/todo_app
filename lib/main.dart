b25843de67224f1f795d3500ec5f222a462d7dde
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-11-14 21:11:34 +0530

    feat(theme): setup global ThemeData and move colors and str & constants to separate folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+Future<void> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hive Todo App',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+          titleMedium: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+          displayMedium: TextStyle(color: Colors.white, fontSize: 21),
+          displaySmall: TextStyle(
+            color: Color.fromARGB(255, 234, 234, 234),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF0097A7);
+  static const List<Color> primaryGradientColor = [
+    Color(0xFF0097A7),
+    Color(0xFF00BCD4),
+  ];
+}
