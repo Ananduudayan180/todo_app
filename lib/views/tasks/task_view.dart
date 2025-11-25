9e35ff1be1105f486f7d4baccea7c2214b74fb48
Anandu Udayan <anandhuudayan180@gamil.com>
2025-11-25 14:27:17 +0530

    Refactor: convert widget to StatefulWidget

--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/views/tasks/widget/task_view_app_bar.dart';
 
-class TaskView extends StatelessWidget {
+class TaskView extends StatefulWidget {
   const TaskView({super.key});
 
   @override
+  State<TaskView> createState() => _TaskViewState();
+}
+
+class _TaskViewState extends State<TaskView> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: TaskViewAppBar());
+    return Scaffold(
+      //AppBar
+      appBar: TaskViewAppBar());
   }
 }
