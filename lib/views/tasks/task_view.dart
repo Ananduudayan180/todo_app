6947dc438fb318f226574fcb17a5196db79aec02
Anandu Udayan <anandhuudayan180@gamil.com>
2025-11-25 17:43:02 +0530

    Build top side texts and extract to method in task_view screen

--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/app_str.dart';
 import 'package:todo_app/views/tasks/widget/task_view_app_bar.dart';
 
 class TaskView extends StatefulWidget {
@@ -11,8 +12,46 @@ class TaskView extends StatefulWidget {
 class _TaskViewState extends State<TaskView> {
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       //AppBar
-      appBar: TaskViewAppBar());
+      appBar: TaskViewAppBar(),
+
+      //body
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            _buildTopSideTexts(textTheme),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildTopSideTexts(TextTheme textTheme) {
+    return SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(width: 100, child: Divider(thickness: 2)),
+                RichText(
+                  text: TextSpan(
+                    text: AppStr.addNewTask,
+                    style: textTheme.titleLarge,
+                    children: [
+                      TextSpan(
+                        text: AppStr.taskStrnig,
+                        style: TextStyle(fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 100, child: Divider(thickness: 2)),
+              ],
+            ),
+          );
   }
 }
