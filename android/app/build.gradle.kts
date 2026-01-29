20f79b43c040a6ab705abd74551244a030551be7
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-29 17:49:23 +0530

    fix: resolve ClassNotFoundException by aligning package name in MainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -39,7 +42,7 @@ android {
         applicationId = "app.ananduudayan.todo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- android/app/src/main/kotlin/com/example/todo_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/todo_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo_app
+package app.ananduudayan.todo
 
 import io.flutter.embedding.android.FlutterActivity
 
